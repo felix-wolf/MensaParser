893d64c2a64f5346751d3739be6b34cf5b919241
Felix Wolf <x3mdroid@gmail.com>
2020-02-05 20:43:30 +0100

    cleanup, combined json entries to a single one, getting closer to extract meals

--- parseMensaSites.py
+++ parseMensaSites.py
@@ -1,12 +1,18 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
+from datetime import datetime
+
 #import webbrowser as browser
 
 BASEURL = "https://speiseplan.studierendenwerk-hamburg.de"
 linksToMensas = []
 linksToMeals = []
 mensaId = 0
+now = datetime.now()
+current_time = now.strftime("%d.%m.%Y %H:%M:%S")
+finalData = [{"time" : current_time, "data" : {}}]
+
 
 html = urlopen("https://www.studierendenwerk-hamburg.de/studierendenwerk/de/essen/speiseplaene/").read()
 mainSite = BeautifulSoup(html, 'html.parser')
@@ -19,23 +25,30 @@ for link in mainSite.find_all('a'):
 #browser.open(linksToMensas[0])
 
 for site in linksToMensas:
-	html1 = urlopen(site)
-	singlePage = BeautifulSoup(html1, 'html.parser')
+	html = urlopen(site)
+	singlePage = BeautifulSoup(html, 'html.parser')
 	for link in singlePage.find_all('a'):
 		if "Diese Woche" in link.get_text():
 			linksToMeals.append(link.get('href'))
 
 for site in linksToMeals:
 	site = BASEURL + site
-	html1 = urlopen(site)
-	singlePage = BeautifulSoup(html1, 'html.parser')
+	html = urlopen(site)
+	singlePage = BeautifulSoup(html, 'html.parser')
 	data = {}
 	data['id'] = mensaId
 	for title in singlePage.find_all('h1'):
 		data['name'] = title.get_text()
+	for dish in singlePage.find_all('p'):
+		classTag = dish.get('class')
+		if classTag != None and 'dish' in classTag:
+			print(dish)
 
 	json_data = json.dumps(data)
-
-	with open('json/json' + str(mensaId) + '.txt', 'w') as f:
-			print(json_data, file = f)
+	finalData[0]['data'][mensaId] = json_data
 	mensaId += 1
+
+
+with open('dataJSON.txt', 'w') as f:
+		print(finalData, file = f)
+
